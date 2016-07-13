51948e9a9efdf2b5fc5c2330d9fe8351460d622e
Aafke Smal <aafke.smal@gmail.com>
2016-07-14 01:14:23 +0200

    party and moving bike with tring

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,13 +1,29 @@
-function swapImages() {
-  var animationbike1 = $(".home-bike-2").removeClass("hidden");
-  var animationbike2 = setTimeout(function() {$(".home-bike-2").addClass("hidden");}, 200 )
-  var animationbike3 = setTimeout(function() {$(".home-bike-2").removeClass("hidden");}, 400 )
-  var animationbike4 = setTimeout(function() {$(".home-bike-2").addClass("hidden");}, 600 )
-  animationbike1
-   animationbike2
-    animationbike3
-     animationbike4
+// function tringTring() {
+//   var animationbike1 = $(".home-bike-2").removeClass("hidden");
+//   var animationbike2 = setTimeout(function() {$(".home-bike-2").addClass("hidden");}, 200 )
+//   var animationbike3 = setTimeout(function() {$(".home-bike-2").removeClass("hidden");}, 400 )
+//   var animationbike4 = setTimeout(function() {$(".home-bike-2").addClass("hidden");}, 600 )
+//   animationbike1
+//    animationbike2
+//     animationbike3
+//      animationbike4
+// }
+
+// function moveBike(){
+//   $("#home-locations-image").animate("margin-left", "300px")
+// }
+
+function partyParty() {
+  var animationparty1 = $(".couch-party").removeClass("hidden");
+  var animationparty2 = setTimeout(function() {$(".couch-party").addClass("hidden");}, 300 )
+  var animationparty3 = setTimeout(function() {$(".couch-party").removeClass("hidden");}, 400 )
+  var animationparty4 = setTimeout(function() {$(".couch-party").addClass("hidden");}, 1200 )
+  animationparty1
+   animationparty2
+    animationparty3
+     animationparty4
 }
+
 
 
 $(document).ready(function(){
@@ -29,11 +45,13 @@ setInterval(function(){
 }, 6000);
 
 
+
+
 // move the word "locations"
 $(".open-locations").on("mouseenter", function(){
   $(".item-locations").css("bottom", "-73px");
-  $("#home-locations-image").show("slide", {direction: "left"}, 2000);
-  setInterval(swapImages, 6000);
+  // $("#home-locations-image").show("slide", {direction: "left"}, "slow");
+  // setInterval(tringTring, 6000);
 });
 
 $(".open-locations").on("mouseleave", function(){
@@ -45,6 +63,7 @@ $(".open-locations").on("mouseleave", function(){
 $(".open-events").on("mouseenter", function(){
   $(".item-events").css("bottom", "-136px");
   $(".home-couch").effect("bounce", "slow");
+  setInterval(partyParty, 8000);
 });
 
 $(".open-events").on("mouseleave", function(){
