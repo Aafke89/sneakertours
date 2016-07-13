ee9ed21863090eea986c96afb6d80af148fda40e
Aafke Smal <aafke.smal@gmail.com>
2016-07-13 16:24:15 +0200

    render menu everywhere except on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
+  before_action :disable_nav, only: :home
 
   def home
 
+  end
+
+  private
+
+  def disable_nav
+    @disable_nav = true
   end
 end
