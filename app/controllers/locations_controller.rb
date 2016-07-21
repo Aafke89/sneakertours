794893b6061f1ee0b32725b4b617f826bfb5a4fe
Aafke Smal <aafke.smal@gmail.com>
2016-07-21 13:32:25 +0200

    Add a new location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -19,23 +19,27 @@ class LocationsController < ApplicationController
     @events = @location.events.where("end_date >= ?", Time.zone.now.beginning_of_day)
   end
 
-  # def new
-  #   @location = Location.new
-  # end
+  def new
+    @location = Location.new
+  end
 
-  # def create
-  #   @location = Location.new(location_params)
-  #   if @location.save
-  #     redirect_to location_path(@location)
-  #   else
-  #     render: new
-  #   end
-  # end
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to location_path(@location)
+    else
+      render :new
+    end
+  end
 
   private
 
   def find_location
     @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:location).permit(:name, :description, :category, :picture, :link, :address)
   end
 
   def filter_locations
@@ -46,9 +50,5 @@ class LocationsController < ApplicationController
       @locations = Location.all
     end
   end
-
-  # def location_params
-  #   params.require(:location).permit(:name, :description, :category, :picture, :link, :address)
-  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :locations, only: [:show, :index]
+  resources :locations, only: [:show, :index, :new, :create]
 
   resources :events, only: [:show, :index, :new, :create]
 end
