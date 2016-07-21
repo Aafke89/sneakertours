9decd8fd35643dd6b9ccf34f994138e32092f070
Aafke Smal <aafke.smal@gmail.com>
2016-07-21 14:03:42 +0200

    Validate new location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,25 @@
 class Location < ActiveRecord::Base
 
+  # relations
   has_many :events
+
+  #validations
+  validates :name, presence: true, length: {minimum: 2, maximum: 30}, uniqueness: true
+  validates :description, presence: true, length: {minimum: 50}
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :latitude, :presence  => {message: "Please fill in a correct streetadress"}
+  validates :longitude, :presence  => {message: "Please fill in a correct streetadress"}
+
+  validates_format_of :link, with: /\A#{URI::regexp}\z/, :message => "Please fill in a correct adress including http:// of https://"
+  validates_format_of :picture, with: /\A#{URI::regexp}\z/, :message => "Please fill in a correct adress including http:// of https://"
+
+
+
+
 
 
   geocoded_by :address
-  after_validation :geocode, if: :address_changed?
+  before_validation :geocode, if: :address_changed?
 
 end
