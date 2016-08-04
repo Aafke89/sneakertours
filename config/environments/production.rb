09ac572d558076c6b336f8b0fe6100faa27db993
Aafke Smal <aafke.smal@gmail.com>
2016-08-04 17:37:43 +0200

    bwah

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
 
     config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: ENV['HOST'] }
+  config.action_mailer.raise_delivery_errors = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
