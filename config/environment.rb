510b75d937aa0f23d11fa5ee00d69622bf9f22fd
Aafke Smal <aafke.smal@gmail.com>
2016-08-04 21:30:47 +0200

    Add metadata

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+# Initialize default meta tags.
+DEFAULT_META = YAML.load_file(Rails.root.join('config/meta.yml'))
+
